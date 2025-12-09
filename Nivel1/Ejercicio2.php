3528da06bdb288acc1ed3538b5a5f6bf368ee207
francobrida <francobrida@gmail.com>
2025-12-09 18:54:34 +0100

    Revision1

--- Nivel1/Ejercicio2.php
+++ Nivel1/Ejercicio2.php
@@ -2,7 +2,7 @@
 
 class Computer{
 
-    public function think() {
+    public function think() : string {
         return "I'm thinking. Using the function ". __FUNCTION__ .", inside the class " . __CLASS__;
     }
 }
--- Nivel1/Ejercicio3.php
+++ Nivel1/Ejercicio3.php
@@ -4,12 +4,12 @@ class Student {
 
     private string $name = "John Doe";
 
-    public function __toString()
+    public function __toString() : string
     {
        return "Im a student object";
     }
 
-    public function __get($name)
+    public function __get($name) : string
     {
         return "name property is private";
     }
