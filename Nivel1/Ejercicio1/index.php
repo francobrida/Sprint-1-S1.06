eaed4ca38d2d1d8c7805177def257ea81e4eac93
francobrida <francobrida@gmail.com>
2025-12-16 15:22:08 +0100

    Corrected Nivel1 Ejercicio3 - right use of magic methods

--- Nivel1/Ejercicio1/index.php
+++ Nivel1/Ejercicio1/index.php
@@ -6,7 +6,7 @@ if (isset($_POST["username"]) && isset($_POST["number"])){
 $_SESSION["username"] = $_POST["username"];
 $_SESSION["number"] = $_POST["number"];
    
-    echo $_SESSION["username"] . " " . $_SESSION["number"];
+echo $_SESSION["username"] . " " . $_SESSION["number"];
 } 
 
 ?>
--- Nivel1/Ejercicio3/index.php
+++ Nivel1/Ejercicio3/index.php
@@ -2,7 +2,7 @@
 require_once "Student.php";
 
 $newStudent = new Student();
-echo $newStudent->__toString() . "<br>"
-. $newStudent->__get("name");
+echo $newStudent . "<br>"
+. $newStudent->name;
 
 ?>
