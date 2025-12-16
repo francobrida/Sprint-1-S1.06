d3f6b04681d34ebebcf551c4cd79c60d71fe7436
francobrida <francobrida@gmail.com>
2025-12-16 15:25:56 +0100

    Created a separate PHP file for Nivel2 Ejercicio2 Turbo trait

--- Nivel2/Ejercicio2/index.php
+++ Nivel2/Ejercicio2/index.php
@@ -1,12 +1,5 @@
 <?php
 require_once "Car.php";
-// Trait created
-trait Turbo {
-
-    public function boost() : string{
-        return "Turbo Started";
-    }
-}
 
 $car1 = new Car("Ford", "1234-ABC", "Gasoline", 220);
 echo $car1->boost();
